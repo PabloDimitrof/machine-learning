f0ffe5854b14290f5bc1c26bc2a99d6fc370a4f9
PabloDimitrof <pablodimitrof@gmail.com>
2025-08-26 10:40:23 -0300

    commit parcial

--- docs/arvore-decisao/decision-tree.py
+++ docs/arvore-decisao/decision-tree.py
@@ -1,2 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
+# Carregar o dataset
+data = pd.read_csv('src/mydata.csv')
+
+# Selecionar características e alvo
+print(data.head(1))
